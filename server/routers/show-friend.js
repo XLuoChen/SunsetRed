e2c4cad5d486b88097f0a14f8dcdec6fba830e5e
DH-TB <929659475@qq.com>
2016-08-22 23:11:06 +0800

    fixed bug

--- server/routers/show-friend.js
+++ server/routers/show-friend.js
@@ -5,9 +5,12 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/friend', (req, res)=> {
-  findData((result) => {
+    let result = [
+      {name: "高乐", sex: "女", city: "西安"},
+      {name: "黄丽珍", sex: "女", city: "西安"},
+      {name: "赵路", sex: "女", city: "西安"},
+      {name: "刘一林", sex: "女", city: "西安"}];
     res.json(result);
-  });
 });
 
 module.exports = router;
