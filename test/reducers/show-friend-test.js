ba337582fee02c42a47633e8b8c2a903aa94d9fb
DH-TB <929659475@qq.com>
2016-08-22 23:01:17 +0800

    modify describe information

--- test/reducers/show-friend-test.js
+++ test/reducers/show-friend-test.js
@@ -1,8 +1,8 @@
 const reducer = require('../../public/js/reducers/show-friend-test');
 const expect = require('chai').expect;
 
-describe('reducer', function () {
-  it('get',function () {
+describe('friend', function () {
+  it('can show',function () {
     const state = {value: ''};
     const action={type:"GETTVALUE",value:{name:"gaole",sex:"女",city:"西安"}};
     expect(reducer(state,action)).to.be.deep.equal({value:{name:"gaole",sex:"女",city:"西安"}});
