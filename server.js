539de2b71df880a0f4117dbe975b05057908c861
DH-TB <929659475@qq.com>
2016-08-22 21:30:37 +0800

    shoe default friend (api and reducer).test

--- server.js
+++ server.js
@@ -4,12 +4,13 @@ const bodyParser = require('body-parser');
 const app = new express();
 
 const hello = require('./server/routers/hello-world');
-
+const friend = require('./server/routers/show-friend');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('./public'));
 
 app.use('/', hello);
+app.use('/',friend);
 
 var server = app.listen(3000, function () {
   console.log('listening at port %s', server.address().port);
