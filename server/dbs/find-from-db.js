8aa281dc4dac706415a962bc29c8d077338cdec4
zhangtiantian10 <975571814@qq.com>
2016-08-22 12:57:23 +0800

    seperate datebase

--- server/dbs/find-from-db.js
+++ server/dbs/find-from-db.js
@@ -1,14 +1,25 @@
-const MongoClient = require('mongodb').MongoClient;
+var config = require('config');
+const db = require('mongodb').MongoClient;
+var dbConfig = config.get('Customer.dbConfig');
 
-const url = 'mongodb://localhost:27017/sunset';
+const url = `mongodb://localhost:27017/${dbConfig.dbName}`;
 
 function findData(callback) {
-  MongoClient.connect(url, function (err, db) {
+  db.connect(url, function (err, db) {
     const collection = db.collection('hello');
     collection.find({}).toArray(function (err, result) {
       callback(result[0].hello);
     });
   });
 }
+
+// function findData(callback) {
+//   MongoClient.connect(url, function (err, db) {
+//     const collection = db.collection('hello');
+//     collection.find({}).toArray(function (err, result) {
+//       callback(result[0].hello);
+//     });
+//   });
+// }
 
 module.exports = findData;
--- test/apis/find.test.js
+++ test/apis/find.test.js
@@ -1,8 +1,20 @@
 const findData = require('../../server/dbs/find-from-db');
 const expect = require('chai').expect;
+var config = require('config');
+const db = require('mongodb').MongoClient;
+var dbConfig = config.get('Customer.dbConfig');
+
+const url = `mongodb://localhost:27017/${dbConfig.dbName}`;
 
 describe('GET /', function () {
 
+  beforeEach(() => {
+    db.connect(url,(err,db)=>{
+      const collection = db.collection('hello');
+      collection.insert([{hello:"world"}],(err,result)=>{
+      })
+    })
+  });
   it('hello', function () {
     findData((result) => {
       expect(result).to.be.equal('world');
