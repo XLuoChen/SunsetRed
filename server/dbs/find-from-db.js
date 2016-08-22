eb46815b21f5f5a15089a902eaafdbab2c890f9b
zhangtiantian10 <975571814@qq.com>
2016-08-22 15:34:01 +0800

    Modify test files

--- server/dbs/find-from-db.js
+++ server/dbs/find-from-db.js
@@ -1,25 +1,13 @@
-var config = require('config');
-const db = require('mongodb').MongoClient;
-var dbConfig = config.get('Customer.dbConfig');
-
-const url = `mongodb://localhost:27017/${dbConfig.dbName}`;
+const url = require('../helpers/get-url');
+const MongoClient = require('../helpers/mongodb');
 
 function findData(callback) {
-  db.connect(url, function (err, db) {
+  MongoClient.connect(url, function (err, db) {
     const collection = db.collection('hello');
     collection.find({}).toArray(function (err, result) {
       callback(result[0].hello);
     });
   });
 }
-
-// function findData(callback) {
-//   MongoClient.connect(url, function (err, db) {
-//     const collection = db.collection('hello');
-//     collection.find({}).toArray(function (err, result) {
-//       callback(result[0].hello);
-//     });
-//   });
-// }
 
 module.exports = findData;
--- test/apis/server.test.js
+++ test/apis/server.test.js
@@ -1,21 +1,32 @@
 const request = require('supertest');
+const url = require('../../server/helpers/get-url');
+const mongoClient = require('../../server/helpers/mongodb');
 
 describe('server', () => {
-   let server;
-
+  let server;
+  
   beforeEach(function () {
+    mongoClient.connect(url, (err, db)=> {
+      const collection = db.collection('hello');
+      collection.insert([{hello: "world"}], (err, result)=> {
+      });
+      db.close();
+    });
     server = require('../../server');
   });
 
-  // afterEach(function (done) {
-  //   server.close(done);
-  // });
+  afterEach(function () {
+    mongoClient.connect(url, (err, db)=> {
+      const collection = db.collection('hello');
+      collection.removeMany({});
+      db.close();
+    })
+  });
 
-  it('responds to /hello', function testSlash(done) {
-    this.timeout(25000);
+  it('responds to /hello', function testSlash() {
     request(server)
       .get('/hello')
-      .expect(200, done);
+      .expect(200, '"world"');
 
   });
 
