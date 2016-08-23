e3fb9469944e1bd0a75463ec24e5470b345eac6b
preservance717 <GL717@outlook.com>
2016-08-23 21:41:17 +0800

    show default friends

--- public/js/components/FriendList.js
+++ public/js/components/FriendList.js
@@ -4,8 +4,10 @@ export default class FriendList extends Component {
   render() {
     return <div>
       {this.props.value.map((ele, index)=> {
-        return<div key={index}>
-          {"姓名" + ele.name + "性别" + ele.sex + "兴趣" + ele.hobbies}
+        return <div key={index} className="box">
+          <a>
+            <img className="friendImg" src={"../../images/" + ele.imgName}/>
+            <span className="www-A1">{ele.name + " " + ele.sex + " " + ele.hobbies}</span></a>
         </div>
       })}
     </div>
--- public/js/components/MakeFriends.js
+++ public/js/components/MakeFriends.js
@@ -2,22 +2,23 @@ import React, {Component} from 'react';
 import FriendList from '../containers/FriendList'
 import {store} from '../main';
 
-export default class MakeFriends extends Component{
-  componentDidMount(){
-    store.dispatch({type:'SET_FRIENDS'})
+export default class MakeFriends extends Component {
+  componentDidMount() {
+    store.dispatch({type: 'SET_FRIENDS'})
   }
-  render(){
-    return <div>
+
+  render() {
+    return <div className="friendWhole">
       <Tabs />
       <FriendList/>
     </div>
   }
 }
- class Tabs extends Component{
-  render(){
+class Tabs extends Component {
+  render() {
     return <div>
-      <span className="findFriendConditon" >根据帐号查找</span>
-      <span className="findFriendConditon" >根据条件查找</span>
+      <span className="findFriendConditon">根据帐号查找</span>
+      <span className="findFriendConditon">根据条件查找</span>
     </div>
   }
 }
