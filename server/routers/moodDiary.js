afca71aba4d5adec87f650ed5db78b6a3dc07c9f
code <1165444511@qq.com>
2016-08-23 10:29:10 +0800

    moodDairy api test by mongo

--- server/routers/moodDiary.js
+++ server/routers/moodDiary.js
@@ -1,14 +1,13 @@
-// const findData = require('../dbs/find-from-db');
+const findData = require('../dbs/moodDiary');
 
 const express = require('express');
 
 const router = express.Router();
 
 router.get('/moodDiaries', (req, res)=> {
-  // findData((result) => {
-    result = [{name:'baiying',title:'我的心情',content:'我很好',sort:'情感天地'}];
+  findData((result) => {
     res.json(result);
-  // });
+  });
 });
 
 module.exports = router;
