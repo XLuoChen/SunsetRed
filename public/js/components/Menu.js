9125556572676e3d33e7b7e3d15be1bc55bc863e
fat-gril <15809244435@163.com>
2016-08-23 20:36:44 +0800

    add traveller menu

--- public/js/components/Menu.js
+++ public/js/components/Menu.js
@@ -1,9 +1,17 @@
-import React,{Component} from 'react';
+import React, {Component} from 'react';
+import {Link} from 'react-router';
 
-export default class Menu extends Component{
-    render(){
-        return <div>
-            Menu
-        </div>
-    }
+export default class Menu extends Component {
+  render() {
+    return <div className="col-lg-offset-2">
+      <nav className="navbar">
+        <ul className="nav navbar-nav navbar-left">
+          <Link to='/' className="navbar-brand menuSize">最新专栏</Link>
+          <Link to='/' className="navbar-brand menuSize"> 最热专栏</Link>
+          <Link to='/' className="navbar-brand menuSize"> 热门话题</Link>
+          <Link to='/' className="navbar-brand publishArticle"> 发表</Link>
+        </ul>
+      </nav>
+    </div>
+  }
 }
