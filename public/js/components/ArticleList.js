f0dda026742ab4cfb0737302b5f9d7feafc8e9b0
XLuoChen <1425145741@qq.com>
2016-08-24 16:39:22 +0800

    save images path in db

--- public/js/components/ArticleList.js
+++ public/js/components/ArticleList.js
@@ -10,16 +10,18 @@ export default class ArticleList extends Component {
     const articles = this.props.articles.map((article, index)=> {
       return <div key={index} className="col-lg-8 col-lg-offset-1 articleList" id="article">
         <h3><a>{article.title}</a></h3>
-        <div className="col-lg-3">
-          <img src="../../images/traveller/artist.jpg" id="picture"/>
+        <div className="col-lg-3" id="parentOfImg">
+          <img src={`../../images/traveller/${article.image}`} id="picture"/>
         </div>
         <div className="col-sm-offset-4">
           {article.content}
           <div id="articleFooter">
             <div className="sameLine">By <a>{article.author}</a></div>
             <div className="sameLine col-md-offset-1">{article.date}</div>
-            <div className="col-lg-offset-6 sameLine"><span className="glyphicon glyphicon-thumbs-up">{article.likedCount}</span></div>
-            <div className="col-sm-offset-1 sameLine"><span className="glyphicon glyphicon-comment">{article.commentCount}</span></div>
+            <div className="col-lg-offset-6 sameLine"><span
+              className="glyphicon glyphicon-thumbs-up">{article.likedCount}</span></div>
+            <div className="col-sm-offset-1 sameLine"><span
+              className="glyphicon glyphicon-comment">{article.commentCount}</span></div>
           </div>
         </div>
       </div>
