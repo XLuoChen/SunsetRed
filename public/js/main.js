7d1f4db95d9d1c1f5e4edd3a64c2b99e629fd366
code <1165444511@qq.com>
2016-08-24 10:22:29 +0800

    show moodDiarylist in page

--- public/js/main.js
+++ public/js/main.js
@@ -1,19 +1,19 @@
 import React, {Component} from "react";
 import {render} from "react-dom";
-import {createStore, applyMiddleware} from 'redux';
+import {createStore, applyMiddleware} from "redux";
 import {Provider} from "react-redux";
-import {Router, Route, IndexRoute, hashHistory} from 'react-router';
+import {Router, Route, IndexRoute, hashHistory} from "react-router";
 import App from "./containers/App";
-import HomePage from './components/HomePage';
-import MakeFriends from './containers/MakeFriends';
-import getFriendsInformation from './middlewares/get-friend-information';
-import reducer from './reducers/index';
-import HappinessShare from './containers/HappinessShare';
+import HomePage from "./components/HomePage";
+import MakeFriends from "./containers/MakeFriends";
+import getFriendsInformation from "./middlewares/get-friend-information";
+import reducer from "./reducers/index";
+import HappinessShare from "./containers/HappinessShare";
 import setHappiness from "./middlewares/set-happiness";
 import MoodDiaries from "./containers/MoodDiaries";
-import Traveller from './containers/Traveller';
-
-const createStoreWithMiddleware = applyMiddleware(getFriendsInformation,setHappiness)(createStore);
+import Traveller from "./containers/Traveller";
+import MooddDiarylists from "./middlewares/get-diaryLists";
+const createStoreWithMiddleware = applyMiddleware(getFriendsInformation,setHappiness, MooddDiarylists)(createStore);
 
 const store = createStoreWithMiddleware(reducer);
 
