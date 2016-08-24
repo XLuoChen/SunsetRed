8411589fe16104a57596023a27dbbdf3345393d0
XLuoChen <1425145741@qq.com>
2016-08-24 10:28:35 +0800

    get articles from db

--- public/js/middlewares/get-articleList.js
+++ public/js/middlewares/get-articleList.js
@@ -1,10 +1,9 @@
-import {request} from 'superagent';
+import request from 'superagent';
 
 export default store => next => action => {
   if (action.type === 'GET_ARTICLES') {
     request.get('/traveller')
       .end((err, res) => {
-        // console.log(res.body);
         next({type: 'ARTICLE_LIST', articles: res.body});
       });
   }
