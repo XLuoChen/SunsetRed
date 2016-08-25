4ed12b4c87841083ae065f78a763dcde5c922488
XLuoChen <1425145741@qq.com>
2016-08-25 19:04:06 +0800

    add eslintignore and ignore mocha when run

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,8 @@ module.exports = {
     "browser": true,
     "commonjs": true,
     "es6": true,
-    "node": true
+    "node": true,
+    "mocha": true
   },
   "extends": "eslint:recommended",
   "parserOptions": {
@@ -17,7 +18,8 @@ module.exports = {
     "React": true
   },
   "plugins": [
-    "react"
+    "react",
+    "mocha"
   ],
   "rules": {
     "prefer-arrow-callback": "error",
